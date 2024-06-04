96340d80e29f0f6cb2c8324f5128885645a2955d
zqz <qingzhe.zhou@yingxiong.com>
2024-06-04 10:06:31 +0800

    [feat] record word

--- src/word_record/__init__.py
+++ src/word_record/__init__.py
@@ -12,31 +12,6 @@ with open("config.json", "r") as f:
     config = json.load(f)
     MOONSHOT_API_KEY = config["MOONSHOT_API_KEY"]
 
-
-def get_select_text():
-    cache = pyperclip.paste()
-
-    control = keyboard.Controller()
-    with control.pressed(keyboard.Key.ctrl):
-        control.press("c")
-        control.release("c")
-    time.sleep(0.1)
-    text = pyperclip.paste()
-    pyperclip.copy(cache)
-    return text
-
-
-def on_click(x, y, button, pressed):
-    global drag_flag, mouse_press, mouse_release, record_txt
-    if pressed:
-        mouse_press = (x, y)
-        return
-    else:
-        mouse_release = (x, y)
-        if mouse_press != mouse_release:
-            record_txt = get_select_text()
-
-
 from plyer import notification
 from g4f.client import Client
 import g4f.debug
@@ -69,16 +44,53 @@ def request_interpret_g4f(word, context):
     return response.choices[0].message.content
 
 
-def request_interpret_moonshot(word,context):
-    from openai import OpenAI
-    
+
+
+def write_to_jsonl(word, context, interpret):
+    obj = {
+        "word": word,
+        "context": context,
+        "interpret": interpret,
+        "time": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+    }
+    json_obj = json.dumps(obj, ensure_ascii=False, indent=4)
+    with open("word_record.jsonl", "a") as f:
+        f.write(json_obj + "\n")
+
+
+def check_valid(text):
+    text = text.strip()
+    if text == "":
+        print("Invalid input", text)
+        return False
+    return True
+
+def retry_request(times: int, interval: int = 1):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for i in range(times):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print('retry request')
+                    time.sleep(interval)
+                    continue
+            raise Exception('retry request failed')
+        return wrapper
+    return decorator
+
+from openai import OpenAI
+
+@retry_request(3, 3)
+def request_interpret_moonshot(word, context):
+
     client = OpenAI(
         api_key=MOONSHOT_API_KEY,
         base_url="https://api.moonshot.cn/v1",
     )
-    
+
     completion = client.chat.completions.create(
-        model="moonshot-v1-32k",
+        model="moonshot-v1-8k",
         messages=[
             {
                 "role": "user",
@@ -86,7 +98,7 @@ def request_interpret_moonshot(word,context):
                     [
                         "Please interpret the meaning of the word {",
                         word,
-                        "} in the context {",
+                        "} in this context {",
                         context,
                         "}",
                         "{",
@@ -101,99 +113,20 @@ def request_interpret_moonshot(word,context):
     return completion.choices[0].message.content
 
 
-
-class WordRecord:
-    def __init__(self) -> None:
-        self._word: str = ""
-        self._context: str = ""
-        self._interpret: str = ""
-        self._record_txt: str = ""
-        self._mouse_press = (0, 0)
-        self._mouse_release = (0, 0)
-
-    def _get_select_text(self):
-        cache = pyperclip.paste()
-
-        control = keyboard.Controller()
-        with control.pressed(keyboard.Key.ctrl):
-            control.press("c")
-            control.release("c")
-        time.sleep(0.1)
-        text = pyperclip.paste()
-        pyperclip.copy(cache)
-        return text
-
-    def on_click(self, x, y, button, pressed):
-        if pressed:
-            self._mouse_press = (x, y)
-            self._record_txt = ""
-        else:
-            self._mouse_release = (x, y)
-            self._record_txt = self._get_select_text()
-
-    def _show_notification(self, title, message):
-        print("show_notification")
-        notification.notify(
-            title=title,
-            message=message,
-            app_icon=None,
-            timeout=1,
-        )
-
-    def record_word(self):
-        print("record_word")
-        self._word = self._record_txt
-
-    def record_context(self):
-        print("record_context")
-        self._context = self._record_txt
-
-        if self._word == "" or self._context == "":
-            self._show_notification("错误", "未选择单词或Context")
-        else:
-            self._interpret = request_interpret_moonshot(self._word, self._context)
-            self._show_notification(
-                "记录成功",
-                "".join(
-                    [
-                        "单词：",
-                        self._word,
-                        "\n",
-                    ]
-                ),
-            )
-            self._write_to_jsonl()
-        self._word = ""
-        self._context = ""
-        self._interpret = ""
-
-    def _write_to_jsonl(self):
-        obj = {
-            "word": self._word,
-            "context": self._context,
-            "interpret": self._interpret,
-        }
-        json_obj = json.dumps(obj)
-        with open("record.jsonl", "a") as f:
-            f.write(json_obj + "\n")
-
-
 def main():
-    word_record = WordRecord()
-    listener = mouse.Listener(
-        on_click=lambda x, y, button, pressed: word_record.on_click(
-            x, y, button, pressed
-        )
-    )
-    listener.start()
-    with keyboard.GlobalHotKeys(
-        {
-            "<ctrl>+<shift>+<alt>+q": lambda: word_record.record_word(),
-            "<ctrl>+<shift>+<alt>+w": lambda: word_record.record_context(),
-        }
-    ) as h:
-        h.join()
-
+    while True:
+        word = input("Please input the word you want to record:")
+        if not check_valid(word):
+            continue
+        context = input("Please input the context:")
+        if not check_valid(word):
+            continue
+        try:
+            interpret = request_interpret_moonshot(word, context)
+        except Exception as e:
+            continue
+        print(interpret)
+        write_to_jsonl(word, context, interpret)
 
 if __name__ == "__main__":
     main()
